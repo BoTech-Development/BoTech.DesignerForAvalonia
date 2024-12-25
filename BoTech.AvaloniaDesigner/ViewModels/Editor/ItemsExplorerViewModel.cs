fd8e2832e1419947a15e5da61bd2a77c353e7cfb
Florian Teetz <fteetz@outlook.de>
2024-12-25 08:23:50 +0100

    Feature #8 is now fully implemented.

--- BoTech.AvaloniaDesigner/ViewModels/Editor/ItemsExplorerViewModel.cs
+++ BoTech.AvaloniaDesigner/ViewModels/Editor/ItemsExplorerViewModel.cs
@@ -1,13 +1,19 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Linq;
+using System.Reflection;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 using BoTech.AvaloniaDesigner.Controller.Editor;
 
 namespace BoTech.AvaloniaDesigner.ViewModels.Editor;
 
 public class ItemsExplorerViewModel : ViewModelBase
 {
-    public ObservableCollection<TreeViewNode> TreeViewNodes { get; } 
+    public ObservableCollection<TreeViewNode> TreeViewNodes { get; set;  } 
     public TreeViewNode? SelectedItem { get; set; }
     
     // will be injected
@@ -16,34 +22,66 @@ public class ItemsExplorerViewModel : ViewModelBase
     public ItemsExplorerViewModel(PreviewController previewController)
     {
         _previewController = previewController;
+      
+       
+        CreateTreeView();
+    }
+
+    private void CreateTreeView()
+    {
+        // Gets all Types which are nested under the Avalonia.? Namespace
+        List<TypeInfo> allTypes = Assembly.Load(new AssemblyName("Avalonia.Controls")).DefinedTypes.ToList();
+        // Therefore it is needed to get all Types which are directly nested under Avalonia.Controls
+        List<TypeInfo> sortedTypes = allTypes.Where(type => type.Namespace == "Avalonia.Controls").ToList();
+        
+        // Because of in the new Sorted List are a lot of Types which we do not use for example Interfaces or Classes like Control, it is necessary to filter all Type which inherit from Control.
+        List<TypeInfo> controlBasedTypes = sortedTypes.Where(type => type.AsType().IsSubclassOf(typeof(Control))).ToList();
+        ObservableCollection<TreeViewNode> nodes = new ObservableCollection<TreeViewNode>();
+        foreach (TypeInfo controlBasedType in controlBasedTypes)
+        {
+            nodes.Add(new TreeViewNode()
+            {
+                Text = controlBasedType.Name,
+                ControlInstance = GetDefaultInstanceForControl(controlBasedType)
+            });
+        }
+
         TreeViewNodes = new ObservableCollection<TreeViewNode>()
         {
             new TreeViewNode()
             {
                 Text = "Avalonia Controls",
                 ControlInstance = null,
-                Children =
-                {
-                    new TreeViewNode()
-                    {
-                        ControlInstance = new Button()
-                        {
-                            Content = "Button",
-                        },
-                        Text = "Button",
-                    },
-                    new TreeViewNode()
-                    {
-                        ControlInstance = new TextBlock()
-                        {
-                            Text = "TextBlock",
-                        },
-                        Text = "TextBlock",
-                    }
-                }
+                Children = nodes
             }
         };
     }
+
+    private Control GetDefaultInstanceForControl(Type controlType)
+    {
+        try
+        {
+            Control? control = Activator.CreateInstance(controlType) as Control;
+            if (control != null)
+            {
+                return control;
+            }
+            return new TextBlock()
+            {
+                Text = "Error by creating an Instance of Type: " + controlType.FullName + " dynamically created Type was null.",
+                Foreground = Brushes.Orange,
+            };
+        }
+        catch (Exception e)
+        {
+            return new TextBlock()
+            {
+                Text = "Error by creating an Instance of Type: " + controlType.FullName + "\n Error Message: " + e.Message,
+                Foreground = Brushes.Orange,
+            };
+        }
+    }
+    
     
     /// <summary>
     /// This Method will be called from the Code behind when the User selects an Item of the Tree View.
@@ -58,7 +96,7 @@ public class ItemsExplorerViewModel : ViewModelBase
     /// </summary>
     public class TreeViewNode
     {
-        public ObservableCollection<TreeViewNode> Children { get; } = new ObservableCollection<TreeViewNode>();
+        public ObservableCollection<TreeViewNode> Children { get; set;  } = new ObservableCollection<TreeViewNode>();
         public string Text { get; set; } = string.Empty;
         /// <summary>
         /// Referenced Control
